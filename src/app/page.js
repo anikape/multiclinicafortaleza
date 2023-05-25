66fe41bbcacd89e98857a2f07b1c9b91a7a66212
Ana Santos <ana.br@hotmail.com>
2023-05-25 12:02:57 -0300

    NEW TESTE

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,11 @@
 
 export default function Home() {
   return (
+    <>
     <h1>Teste</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt earum, quis expedita eaque quae similique tempora nobis maxime nihil, praesentium cumque. Nemo quis magnam fugit rem molestiae, quos veritatis mollitia!</p>
+    <h2>testeeeee</h2>
+    </>
+    
   )
 }
